b62f9883baecaacb5434774e4f9e40855645dbcc
laoxu <1764542037@qq.com>
2021-08-30 19:09:17 +0800

    测试2

---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import VueRouter from 'vue-router'
+
+
+
+//1.安装插件
+Vue.use(VueRouter)
+
+
+const routes = [
+
+]
+
+//创建router
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
+
+
+export default router
+
+
+
+
