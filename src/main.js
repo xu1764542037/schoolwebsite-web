2684a82922bad0ea60986a436b79999c55c98562
laoxu <1764542037@qq.com>
2021-08-30 17:56:58 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from "./store";
+import router from "./router";
+
 
 Vue.config.productionTip = false
 
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
 new Vue({
   render: h => h(App),
+  router,
+  store
 }).$mount('#app')
